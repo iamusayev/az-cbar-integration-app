bcb3279af8fd68d68a77dabfad640202822b6bed
Anar <95879636+iamusayev@users.noreply.github.com>
2022-06-07 21:41:28 +0400

    Update RateMapper.java

--- src/main/java/com/example/azdigitalumbrellacbarintegration/mapper/RateMapper.java
+++ src/main/java/com/example/azdigitalumbrellacbarintegration/mapper/RateMapper.java
@@ -33,20 +33,20 @@ public class RateMapper {
                 .collect(Collectors.toList());
     }
 
-
+    
     public static List<RateEntity> mapValCursToListRateEntities(ValCurs valCurs) {
-        LocalDate localDate = BuildHelper.buildDate(valCurs);
+        LocalDate cbarDate = BuildHelper.buildDate(valCurs);
         List<Valute> metals = valCurs.getValType().get(0).getValute();
         List<Valute> currencies = valCurs.getValType().get(1).getValute();
 
         List<RateEntity> rates = new ArrayList<>();
 
         for (Valute metal : metals) {
-            RateEntity rate = BuildHelper.buildRateEntity(metal, Type.PRECIOUS_METAL, localDate);
+            RateEntity rate = BuildHelper.buildRateEntity(metal, Type.PRECIOUS_METAL, cbarDate);
             rates.add(rate);
         }
         for (Valute currency : currencies) {
-            RateEntity rate = BuildHelper.buildRateEntity(currency, Type.FOREIGN_CURRENCY, localDate);
+            RateEntity rate = BuildHelper.buildRateEntity(currency, Type.FOREIGN_CURRENCY, cbarDate);
             rates.add(rate);
         }
         return rates;
